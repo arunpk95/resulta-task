3a4f0ba2b90477509593b0c73cb3bfc571beb01a
arunpk95 <arunpk95@gmail.com>
2021-10-31 18:59:58 -0400

    adding cors

--- __init__.py
+++ __init__.py
@@ -1,8 +1,11 @@
 from helpers import *
 from flask import Flask
+from flask_cors import CORS
 
 #iniate flask app
 app = Flask(__name__)
+#allow CORS to consume api from the codepen client, currenly allowed to cosume from all orgins
+CORS(app)
 
 #endpoint for the required stats  http://127.0.0.1:5000/getstats/<league_name>/<from_date>/<to_date>
 #sample url: http://127.0.0.1:5000/getstats/NFL/2020-01-12/2020-01-13
